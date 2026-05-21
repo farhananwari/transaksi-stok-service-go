deb02c5f4c2b9a4e7151f520d5c31ebfabe4f0d6
farhananwari <farhananwari1999@gmail.com>
2026-05-21 16:09:45 +0700

    update

--- models/stok_opname_items.model.go
+++ models/stok_opname_items.model.go
@@ -4,9 +4,7 @@ type StokOpnameItems struct {
 	ID                 string `gorm:"type:uuid;default:gen_random_uuid();primaryKey" json:"id"`
 	StokOpnameHeaderID string `gorm:"type:uuid;" json:"stok_opname_header_id"`
 	BarangID           string `gorm:"type:uuid;" json:"barang_id"`
-	QtyBalance         int    `gorm:"type:int;" json:"qty_balance"`
-	QtyOpname          int    `gorm:"type:int;" json:"qty_opname"`
-	QtyDiff            int    `gorm:"type:int;" json:"qty_diff"`
-	AdjustmentStatus   string `gorm:"type:varchar(100);not null;" json:"adj_status"`
+	StokBalancesID     string `gorm:"type:uuid;" json:"stok_balances_id"`
+	StokOpname         int    `gorm:"type:int;" json:"stok_opname"`
 	Catatan            string `gorm:"type:varchar(255);not null;" json:"catatan"`
 }
